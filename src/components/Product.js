88d64ca228809958a4819407daf16668c722d9a9
Telli <medaminetelli@gmail.com>
2022-04-18 10:45:19 +0100

    fix image error

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,7 +2,15 @@ import React, { Component } from "react";
 import styled from "styled-components";
 export default class Product extends Component {
   render() {
-    return <ProductFrame>{this.props.product.name}</ProductFrame>;
+    return (
+      <ProductFrame>
+        <ProductImage src={this.props.product.img}> </ProductImage>
+        <ProductInfoWrapper>
+          <span>{this.props.product.name}</span>
+          <span>{this.props.product.price}</span>
+        </ProductInfoWrapper>
+      </ProductFrame>
+    );
   }
 }
 
@@ -12,4 +20,21 @@ const ProductFrame = styled.div`
   min-height: 150px;
   background-color: rgb(110, 110, 110, 0.7);
   margin: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ProductImage = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 25px;
+`;
+const ProductInfoWrapper = styled.div`
+  margin-top: auto;
+  margin-bottom: 5px;
+  display: flex;
+  flex-direction: column;
+  & > span {
+    text-align: center;
+  }
 `;
